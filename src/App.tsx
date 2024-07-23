1b4a207e2db922446988ec3536594de0b917a5ae
Kuba <kubaz11011@gmail.com>
2024-07-23 23:27:24 +0200

    + update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React, { useState } from "react";
 import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
@@ -9,37 +8,45 @@ import { useTonConnect } from "./hooks/useTonConnect";
 import "@twa-dev/sdk";
 
 const StyledApp = styled.div`
-  background-color: #e8e8e8;
-  color: black;
-
-  @media (prefers-color-scheme: dark) {
-    background-color: #222;
-    color: white;
-  }
+  background-color: #000;
+  color: white;
   min-height: 100vh;
   padding: 20px 20px;
 `;
 
 const AppContainer = styled.div`
-  max-width: 900px;
+  max-width: 360px;
   margin: 0 auto;
+  background: url('src/assets/img/bg1.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding: 20px 15px 100px;
+  position: relative;
+  color: #fff;
+  overflow: hidden;
 `;
+
+const honeycombData = [
+  [
+    { id: 'honey-1'},
+    { id: 'honey-2'},
+  ],
+  [
+    { id: 'honey-3'},
+    { id: 'honey-4', background: 'url(src/assets/img/bg1.png)', isActive: true },
+    { id: 'honey-5'},
+  ],
+  [
+    { id: 'honey-6'},
+    { id: 'honey-7'},
+  ],
+];
 
 const App: React.FC = () => {
   const { network } = useTonConnect();
-  const [activeCell, setActiveCell] = useState<string | null>(null);
 
-  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    const target = event.target as HTMLElement;
-    const dataAttr = target.getAttribute('data-attr');
-    if (dataAttr) {
-      setActiveCell(dataAttr);
-      console.log(dataAttr);
-    }
-  };
-
-  const handleCloseClick = () => {
-    setActiveCell(null);
+  const handleCellClick = (dataAttr: string) => {
+    console.log(dataAttr);
   };
 
   return (
@@ -49,27 +56,42 @@ const App: React.FC = () => {
           <FlexBoxRow>
             <TonConnectButton />
           </FlexBoxRow>
+          <Counter />
           <FlexBoxRow>
-            <div className={`app ${activeCell ? "active-cell" : ""}`}>
-              <div className="honeycomb" onClick={handleClick}>
-                <div className="honeycomb-close" onClick={handleCloseClick}></div>
-                <div className="row top-row">
-                  <div className={`honeycomb-cell ${activeCell === "honey-1" ? "active" : ""}`} data-attr="honey-1"></div>
-                  <div className={`honeycomb-cell ${activeCell === "honey-2" ? "active" : ""}`} data-attr="honey-2"></div>
-                </div>
-                <div className="row middle-row">
-                  <div className={`honeycomb-cell ${activeCell === "honey-3" ? "active" : ""}`} data-attr="honey-3"></div>
-                  <div className={`honeycomb-cell honeycomb-cell--main ${activeCell === "honey-4" ? "active" : ""}`} data-attr="honey-4"></div>
-                  <div className={`honeycomb-cell ${activeCell === "honey-5" ? "active" : ""}`} data-attr="honey-5"></div>
-                </div>
-                <div className="row bottom-row">
-                  <div className={`honeycomb-cell ${activeCell === "honey-6" ? "active" : ""}`} data-attr="honey-6"></div>
-                  <div className={`honeycomb-cell ${activeCell === "honey-7" ? "active" : ""}`} data-attr="honey-7"></div>
-                </div>
+            <div className="app">
+              <div className="honeycomb">
+                {honeycombData.map((row, rowIndex) => (
+                  <div className="row" key={rowIndex}>
+                    {row.map(cell => {
+                      const style = cell.background 
+                        ? { backgroundImage: cell.background } 
+                        : { background: '#fff' };
+                      return (
+                        <div 
+                          className={`honeycomb-cell ${cell.isActive ? "active" : ""}`} 
+                          data-attr={cell.id} 
+                          key={cell.id}
+                          onClick={() => handleCellClick(cell.id)}
+                        >
+                          <div className="honeycomb-cell--single" style={style}></div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                ))}
               </div>
             </div>
           </FlexBoxRow>
-          <Counter />
+        </FlexBoxCol>
+        <FlexBoxCol>
+          <FlexBoxRow>
+            <div className="resources-box width100 text-center">
+              <h3><strong>Resources</strong></h3>
+              <div className="flex">
+                <div></div>
+              </div>
+            </div>
+          </FlexBoxRow>
         </FlexBoxCol>
       </AppContainer>
     </StyledApp>
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,14 +2,6 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 import { useCounterContract } from "../hooks/useCounterContract";
 import { useTonConnect } from "../hooks/useTonConnect";
 
-import {
-  Card,
-  FlexBoxCol,
-  FlexBoxRow,
-  Ellipsis,
-  Button,
-} from "./styled/styled";
-
 export function Counter() {
   const { connected } = useTonConnect();
   const { value, address, sendIncrement } = useCounterContract();
@@ -17,28 +9,6 @@ export function Counter() {
   return (
     <div className="Container">
       <TonConnectButton />
-
-      <Card>
-        <FlexBoxCol>
-          <FlexBoxRow>
-            <b>Address</b>
-            <Ellipsis>{address}</Ellipsis>
-          </FlexBoxRow>
-          <FlexBoxRow>
-            <b>Value</b>
-            <div>{value ?? "Loading..."}</div>
-          </FlexBoxRow>
-          <Button
-            disabled={!connected}
-            className={`Button ${connected ? "Active" : "Disabled"}`}
-            onClick={() => {
-              sendIncrement();
-            }}
-          >
-            Increment
-          </Button>
-        </FlexBoxCol>
-      </Card>
     </div>
   );
 }
