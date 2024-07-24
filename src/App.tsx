d77a21ab83043f56309f107a60cb63aa959018b1
Kuba <kubaz11011@gmail.com>
2024-07-24 21:03:39 +0200

    + change path to images

--- src/App.tsx
+++ src/App.tsx
@@ -30,11 +30,11 @@ const AppContainer = styled.div`
 const honeycombData = [
   [
     { id: 'honey-1'},
-    { id: 'honey-2', background: 'url(/img/bg2.png)', isActive: true},
+    { id: 'honey-2', background: 'url(../img/bg2.png)', isActive: true},
   ],
   [
     { id: 'honey-3'},
-    { id: 'honey-4', background: 'url(/img/bg1.png)', isActive: true },
+    { id: 'honey-4', background: 'url(./img/bg1.png)', isActive: true },
     { id: 'honey-5'},
   ],
   [
@@ -89,8 +89,8 @@ const App: React.FC = () => {
             <div className="resources-box width100 text-center">
               <h3><strong>Resources</strong></h3>
               <div className="resouces-box--values">
-                <div className="resouces-box--values-single"><img src="/img/r1.png" alt="resource1" />104</div>
-                <div className="resouces-box--values-single"><img src="/img/r2.png" alt="resource2" />28</div>
+                <div className="resouces-box--values-single"><img src="../img/r1.png" alt="resource1" />104</div>
+                <div className="resouces-box--values-single"><img src="./img/r2.png" alt="resource2" />28</div>
                 <div className="resouces-box--values-single"><img src="/img/r3.png" alt="resource3" />10</div>
               </div>
             </div>
